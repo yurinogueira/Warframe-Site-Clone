271af008f6782239e4114c102107ce9d6614dba2
yurinogueira <yurinogueira@id.uff.br>
2022-09-10 13:04:15 -0300

    feature: login and register form

--- js/utils.js
+++ js/utils.js
@@ -15,3 +15,9 @@ function openToast(
     let toast = bootstrap.Toast.getOrCreateInstance(toastElement);
     toast.show();
 }
+
+function openRegisterAndCloseLogin() {
+    let loginOffCanvasElement = document.getElementById("registerOffcanvas");
+    let loginOffCanvas = new bootstrap.Offcanvas(loginOffCanvasElement);
+    loginOffCanvas.show();
+}
